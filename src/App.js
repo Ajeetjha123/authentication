1b9d06ba16120bc6e7c17c451c47288bfaade783
Ajeet Kumar Jha <kumarjhaajeet653@gmail.com>
2023-11-09 16:14:18 +0530

    local storage added

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 
-import Layout from './components/Layout/Layout';
-import UserProfile from './components/Profile/UserProfile';
-import AuthPage from './pages/AuthPage';
-import HomePage from './pages/HomePage';
+import Layout from "./components/Layout/Layout";
+import UserProfile from "./components/Profile/UserProfile";
+import AuthPage from "./pages/AuthPage";
+import HomePage from "./pages/HomePage";
+import { useContext } from "react";
+import AuthContext from "./store/context";
 
 function App() {
+  const authCtx = useContext(AuthContext);
   return (
     <BrowserRouter>
       <Layout>
         <Routes>
-          <Route path='/' element={<HomePage />} />
-          <Route path='/auth' element={<AuthPage />} />
-          <Route path='/profile' element={<UserProfile />} />
+          <Route path="/" element={<HomePage />} />
+          {!authCtx.isLoggedIn && <Route path="/auth" element={<AuthPage />} />}
+          {authCtx.isLoggedIn && (
+            <Route path="/profile" element={<UserProfile />} />
+          )}
+          <Route path="*" element={<Navigate to="/" />} />
         </Routes>
       </Layout>
     </BrowserRouter>
